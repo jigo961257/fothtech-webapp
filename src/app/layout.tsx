78abbe40e979a579430c85d9dad81608f48dfb50
Jeegar Goyani <jigo961257@gmail.com>
2024-06-14 21:56:37 +0530

    install-shadcn-fontsetup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ['200', '400', '500', '600', '800']
+});
 
 export const metadata: Metadata = {
   title: "ForthTech",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
