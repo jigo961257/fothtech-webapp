03681a74beb75eeb87e913b267588e06a2e4cdda
Jeegar Goyani <jigo961257@gmail.com>
2024-06-16 00:50:18 +0530

    add:hero-section
    this commit contain the hero section with basic style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@ import { LogoComponent } from "@/components/custom/icons/logo";
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
-      <header className="h-20 shadow-lg shadow-[#FF24241A]bg-white p-2 px-4 sm:p-2 sm:px-8 lg:p-4 lg:px-12">
+      <header className="h-20 drop-shadow-lg shadow-[#3eff24cd] bg-white p-2 px-4 sm:p-2 sm:px-8 lg:p-4 lg:px-12">
         <section className="flex items-center h-full justify-between">
+          <Link href={"/"}>
           <LogoComponent />
+          </Link>
           {/* full screen */}
           <section className="hidden md:flex justify-between items-center">
               <Button  variant={"ghost"} className="hover:text-primary text-[16px] font-medium active:text-primary text-black" >
@@ -17,15 +20,35 @@ export default function Home() {
               <Button  variant={"ghost"} className="hover:text-primary text-[16px] font-medium" >
                 Cotact Us
               </Button>
+              <Button  variant={"ghost"} className="hover:text-primary text-[16px] font-medium" >
+               Service
+              </Button>
+              <Button  variant={"ghost"} className="hover:text-primary text-[16px] font-medium" >
+                Career
+              </Button>
           </section>
           {/* mobile screen */}
-          <section className="flex flex-col items-center justify-center h-full sm:hidden">
+          <section className="flex flex-col items-center justify-center h-full md:hidden">
             <Button variant={'ghost'} size={"icon"}>
               <Menu className="h-8 w-8" />
             </Button>
           </section>
         </section>
       </header>
+      <section className="flex-grow bg-slate-200 justify-center items-center relative">
+        <Image src={"https://static.wikia.nocookie.net/bokunoheroacademia/images/4/40/High-End_Robot.png/revision/latest?cb=20221014170622"} 
+        alt={"hero-section"} 
+        layout="fill"
+          className="flex z-10"
+          objectFit="cover"
+        />
+        <div className="z-20 absolute h-full w-full">
+          <section className="flex flex-col h-full p-4 bg-transparent mx-auto text-white pt-4 items-center justify-center">
+              <span className="text-center text-3xl font-semibold">Forth<span className="text-primary">Tech</span></span>
+              <p className="text-center mt-2 sm:px-20 md:px-40">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+          </section>
+        </div>
+      </section>
     </main>
   );
 }
--- src/components/custom/icons/logo.tsx
+++ src/components/custom/icons/logo.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils"
 import { ClassValue } from "clsx"
 
-export const LogoComponent = ({className} : {className?:ClassValue}) => {
+export const LogoComponent = ({className, } : {className?:ClassValue}) => {
     return (
         <svg width="48" height="52" viewBox="0 0 48 52" fill="none" xmlns="http://www.w3.org/2000/svg" className={cn(className)}>
             <path d="M48 10.4404H0L7.5 0H40.5L48 10.4404ZM7.12154 25.4516L25.5831 52L34.22 46.0601L20.2554 25.9778H40.5015L48.0015 15.5374H0L7.12154 25.4516Z" fill="#FF2424" />
